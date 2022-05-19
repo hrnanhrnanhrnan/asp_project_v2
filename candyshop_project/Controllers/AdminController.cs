5896d179904939ecfddcaf464eac03eb864ee268
Freddy Söderberg <freddy.soderberg@edu.edugrade.se>
2022-05-19 12:55:54 +0200

    Added basic CRUD-pages

--- candyshop_project/Controllers/AdminController.cs
+++ candyshop_project/Controllers/AdminController.cs
@@ -7,10 +7,25 @@ using System.Threading.Tasks;
 
 namespace Candyshop.Controllers
 {
-    [Authorize(Roles="Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateProduct()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteProduct()
         {
             return View();
         }
